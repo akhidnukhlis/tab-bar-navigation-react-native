8bf1ec507f6cca58baa7ddbece80094585908cf0
akhidnukhlis <nukhlis@gmail.com>
2023-09-12 17:30:01 +0700

    first commit

---
+++ src/screens/ChatScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const ChatScreen = ({ navigation }) => {
+    return (
+        <View style={ styles.container }>
+            <Text>Chat Screen</Text>
+        </View>
+    );
+}
+
+export default ChatScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#8fcbbc",
+    }
+})
---
+++ src/screens/SettingScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const SettingScreen = ({ navigation }) => {
+    return (
+        <View style={ styles.container }>
+            <Text>Setting Screen</Text>
+        </View>
+    );
+}
+
+export default SettingScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#8fcbbc",
+    }
+})
